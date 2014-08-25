7981a4d8e92db0705c1fd4f2688944c2e51c82c4
Mario Garrido <skinie18@gmail.com>
2014-08-25 16:35:51 +0100

    Add the database info

---
+++ shortlinks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `shotlinks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) NOT NULL,
+  `url` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+INSERT INTO `shotlinks` (`id`, `code`, `url`, `created`) VALUES
+(1, 'grid', 'http://codegrid.org', '2014-07-02 22:22:48'),
