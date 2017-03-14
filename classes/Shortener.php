9992c4d6757e68b67cb976cec6b694ce9ed5d7bb
Mário Garrido <mrfhitz@users.noreply.github.com>
2017-03-14 12:34:38 +0000

    Fix the database name that was previously wrong

--- classes/Shortener.php
+++ classes/Shortener.php
@@ -25,23 +25,23 @@ class Shortener {
         $url = $this->db->escape_string($url);
         
         // Check if URL already exists
-        $exists = $this->db->query("SELECT code FROM links WHERE url = '{$url}'");
+        $exists = $this->db->query("SELECT code FROM shortlinks WHERE url = '{$url}'");
         
         if($exists->num_rows) {
             return $exists->fetch_object()->code;
         }else {
-            $this->db->query("INSERT INTO links (url, created) VALUES ('{$url}', NOW())");
+            $this->db->query("INSERT INTO shortlinks (url, created) VALUES ('{$url}', NOW())");
         
             $code = $this->generateCode($this->db->insert_id);
         
-            $this->db->query("UPDATE links SET code = '{$code}' WHERE url = '{$url}'");
+            $this->db->query("UPDATE shortlinks SET code = '{$code}' WHERE url = '{$url}'");
             return $code;
         }
     }
     
     public function getUrl($code) {
         $code = $this->db->escape_string($code);
-        $code = $this->db->query("SELECT url FROM links WHERE code = '{$code}'"); 
+        $code = $this->db->query("SELECT url FROM shortlinks WHERE code = '{$code}'"); 
         
         if($code->num_rows) {
             return $code->fetch_object()->url;
